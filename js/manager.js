6a7562c2161f5873b8cdbc0328e2c51140f7db18
Vasil Sokolov <vasil@afterlogic.com>
2022-11-23 18:25:30 +0200

    removed code related to the admin panel

--- js/manager.js
+++ js/manager.js
@@ -11,45 +11,6 @@ module.exports = function (oAppData) {
 	
 	Settings.init(oAppData);
 	
-	if (App.getUserRole() === Enums.UserRole.SuperAdmin)
-	{
-		return {
-			/**
-			 * Registers admin settings tabs before application start.
-			 * 
-			 * @param {Object} ModulesManager
-			 */
-			start: function (ModulesManager)
-			{
-				ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelTab', [
-					function(resolve) {
-						require.ensure(
-							['modules/%ModuleName%/js/views/PerUserAdminSettingsView.js'],
-							function() {
-								resolve(require('modules/%ModuleName%/js/views/PerUserAdminSettingsView.js'));
-							},
-							'admin-bundle'
-						);
-					},
-					Settings.HashModuleName + '-user',
-					TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB_SYNC')
-				]);
-				ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelTab', [
-					function(resolve) {
-						require.ensure(
-							['modules/%ModuleName%/js/views/AdminSettingsView.js'],
-							function() {
-								resolve(require('modules/%ModuleName%/js/views/AdminSettingsView.js'));
-							},
-							'admin-bundle'
-						);
-					},
-					Settings.HashModuleName + '-system',
-					TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')
-				]);
-			}
-		};
-	}
 	if (App.isUserNormalOrTenant())
 	{
 		var
